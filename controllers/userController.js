2c7e86a9b8c5ba02814609a29b1d6b4d0c08337a
mohammad <mohammad@gmail.com>
2025-03-14 10:56:03 +0330

    add test in git

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@
 const getUserController = async (req, res) => {
   res.status(200).send("user data 22");
   console.log(req.body.id);
+  console.log("salam in ro barayee test dar git neveshtam");
 };
 
 module.exports = { getUserController };
