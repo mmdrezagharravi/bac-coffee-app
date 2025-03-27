551c2ae25d0a0a8c256d5a58fc8d22b07065e512
mohammad <mohammad@gmail.com>
2025-03-27 12:08:48 +0330

    add delete cofe api and get all cofe api

--- controllers/cofeController.js
+++ controllers/cofeController.js
@@ -49,4 +49,96 @@ const createCofeController = async (req, res) => {
   }
 };
 
-module.exports = { createCofeController };
+// GET all cofe
+
+const getAllCofeController = async (req, res) => {
+  try {
+    const cofes = await cofeResturantModel.find({});
+    if (!cofes) {
+      return res.status(404).send({
+        success: false,
+        message: "no cofe availible",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      totalCount: cofes.length,
+      cofes,
+    });
+  } catch (error) {
+    console.log(object);
+    res.status(500).send({
+      success: false,
+      message: "error in get all cofe api",
+      error,
+    });
+  }
+};
+// Get cofe by id
+const getCofeByIdController = async (req, res) => {
+  try {
+    const cofeId = req.params.id;
+    if (!cofeId) {
+      return res.status(404).send({
+        success: false,
+        message: "please provide cofe id",
+      });
+    }
+    // find cofe
+    const cofe = await cofeResturantModel.findById(cofeId);
+    if (!cofe) {
+      return res.status(404).send({
+        success: false,
+        message: "no cofe found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      cofe,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in get cofe by id api",
+      error,
+    });
+  }
+};
+
+// Delete cofe
+const deleteCofeController = async (req, res) => {
+  try {
+    const cofeId = req.params.id;
+    if (!cofeId) {
+      return res.status(404).send({
+        success: false,
+        message: "please provide cofe ai",
+      });
+    }
+    await cofeResturantModel.findByIdAndDelete(cofeId);
+    res.status(200).send({
+      success: true,
+      message: "cofe deleted successfully",
+    });
+    if (!cofeId) {
+      return res.status(404).send({
+        success: false,
+        message: "no cofe found .",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "error in delete cofe api",
+    });
+  }
+};
+
+module.exports = {
+  createCofeController,
+  getAllCofeController,
+  getCofeByIdController,
+  deleteCofeController,
+};
--- routes/cofeRoutes.js
+++ routes/cofeRoutes.js
@@ -2,11 +2,25 @@ const express = require("express");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 const { create } = require("../models/userModel");
-const { createCofeController } = require("../controllers/cofeController");
+const {
+  createCofeController,
+  getAllCofeController,
+  getCofeByIdController,
+  deleteCofeController,
+} = require("../controllers/cofeController");
 
 const router = express.Router();
 
 // create cofe || post
 router.post("/create", authMiddleware, createCofeController);
+
+// Get all cofe || GET
+router.get("/getAll", getAllCofeController);
+
+// Get cofe by id || GET
+router.get("/get/:id", getCofeByIdController);
+
+// Delete cofe resturant || Delete
+router.delete("/delete/:id", authMiddleware, deleteCofeController);
 
 module.exports = router;
