53fa592db2e8b2675a3819fcc9598ef4e1da3e80
mohammad <mohammad@gmail.com>
2025-04-14 19:06:31 +0330

    add items : get all category , update category , delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,5 +28,90 @@ const createCatController = async (req, res) => {
     });
   }
 };
+// get all category
+const getAllCategoryController = async (req, res) => {
+  try {
+    const categories = await categoryModel.find({});
+    if (!categories) {
+      res.status(404).send({
+        success: false,
+        message: "not found categories ):",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "its all categories : ",
+      totalCategory: categories.length,
+      categories,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in get all category api ",
+      error,
+    });
+  }
+};
 
-module.exports = { createCatController };
+// update category
+const updateCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, imageUrl } = req.body;
+    const updateCategory = await categoryModel.findByIdAndUpdate(
+      id,
+      { title, imageUrl },
+      { new: true }
+    );
+    if (!updateCategory) {
+      return res.status(404).send({
+        success: false,
+        message: "not found category :(",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "category updated successfully !! :)",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "errror in update category api !!",
+      error,
+    });
+  }
+};
+
+// delete category
+
+const deletCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletCategory = await categoryModel.findByIdAndDelete(id);
+    if (!deletCategory) {
+      return res.status(404).send({
+        success: false,
+        message: "not found category :(",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "category delete successfully :)",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in delete category api !!",
+    });
+  }
+};
+
+module.exports = {
+  createCatController,
+  getAllCategoryController,
+  updateCategoryController,
+  deletCategoryController,
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 // const router = require("./authRoutes");
-const { createCatController } = require("../controllers/categoryController");
+const {
+  createCatController,
+  getAllCategoryController,
+  updateCategoryController,
+  deletCategoryController,
+} = require("../controllers/categoryController");
 
 const router = express.Router();
 
 //router
 // create category
 router.post("/create", authMiddleware, createCatController);
+
+// get all category
+router.get("/getAll", getAllCategoryController);
+
+// update category
+router.put("/update/:id", authMiddleware, updateCategoryController);
+
+// delete category
+router.delete("/delete/:id", authMiddleware, deletCategoryController);
 
 module.exports = router;
